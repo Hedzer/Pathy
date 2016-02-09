f8a2b560f340d7879ac6b632a8a06a3fd3af24c6
Hedzer <hedzer87@gmail.com>
2016-02-08 23:18:40 -0500

    Added include, alias, unalias

--- pathy.js
+++ pathy.js
@@ -14,6 +14,7 @@ var Pathy = (function(window){
 			};
 		}
 	}, 10);
+	var aliases = {};
 	function select(q){
 		return document.querySelector(q);
 	}
@@ -44,8 +45,11 @@ var Pathy = (function(window){
 		}
 		Pathy.navigate(cleaned, args, true);
 	}
-	function run(url, args){
+	function runScripts(url, args){
 		var scripts = select(Pathy.container).getElementsByTagName("script");
+		execute(url, scripts, args);
+	}
+	function execute(url, scripts, args){
 		scripts = Array.prototype.slice.call(scripts,0);
 		if (scripts && scripts.forEach){
 			scripts.forEach(function(script){
@@ -53,7 +57,7 @@ var Pathy = (function(window){
 				//var code = (script.src ? script.src : script.innerText);
 				var code = script.innerText;
 				//add state maybe?
-				window.eval.call(window,"(function($arguments){"+code+" })" + '//# sourceURL='+url)($arguments);
+				window.eval.call(window,"(function($arguments){"+code+" })" + '//# sourceURL='+getAlias(url))($arguments);
 			});
 		}
 	}
@@ -68,6 +72,19 @@ var Pathy = (function(window){
 			});
 		},100);
 	}
+	function getLocation(url){
+		routes = (typeof Pathy.routes == "string" ? Pathy.routes : "./app/routes/").trim();
+		var length = routes.length;
+		if (routes[length - 1] !== "/" ) {
+			routes+="/";
+			Pathy.routes = routes;
+		}
+		var result = routes+getAlias(url)+".html";
+		return result;
+	}
+	function getAlias(url){
+		return (aliases[url] ? aliases[url] : url);
+	}
 	var Pathy = {
 		liveReload:true,
 		routes:"./app/routes/",
@@ -80,18 +97,44 @@ var Pathy = (function(window){
 					window.location.hash = expected;
 				}
 			}
-			var file = Pathy.routes+url+".html";
+			var file = getLocation(url);
 			fs.readFile(file, 'utf8', function (err,data) {
 				if (err) {
 					return console.log(err);
 				}
 				stamp(file);
 				select(Pathy.container).innerHTML = data;
-				run(url, args);
+				runScripts(url, args);
 				if (Pathy.liveReload){
 					monitor(url, args);
 				}
 			});
+		},
+		include:function(url, args){
+			var file = getLocation(url);
+			console.log(file);
+			fs.readFile(file, 'utf8', function (err,data) {
+				if (err) {
+					return console.log(err);
+				}
+				var root = document.createElement("div");
+				root.innerHTML = data;
+				var scripts = root.getElementsByTagName("script");
+				if (root.hasChildNodes()){
+					var children = root.childNodes;
+					for (var i = 0; i < children.length; i++){
+						select(Pathy.container).appendChild(children[i].cloneNode(true));
+					}
+				}
+				execute(url, scripts, args);
+			});
+
+		},
+		alias:function(url, alias){
+			aliases[alias] = url;
+		},
+		unalias:function(alias){
+			delete aliases[alias];
 		}
 	};
 	return Pathy;
